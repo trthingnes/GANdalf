cea599edf63610b7c694a88ad894c5caba12155f
Tobias Rødahl Thingnes <tobbi.rt@gmail.com>
2022-11-02 11:46:46 +0100

    Added better variable names to discriminator

--- src/cgan/dataset/fashion_mnist.py
+++ src/cgan/dataset/fashion_mnist.py
@@ -7,16 +7,16 @@ class FashionMNIST(Dataset):
     def __init__(self, data_path="training_data", transform=None):
         super().__init__()
         self.dataset = torchvision.datasets.FashionMNIST(root=data_path, transform=transform)
-        self.labels = self.dataset.classes
-        self.image_labels = self.dataset.targets
+        self.category_labels = self.dataset.classes
+        self.image_categories = self.dataset.targets
         self.images = self.dataset.data
         self.transform = transform
 
     def __len__(self):
         return len(self.dataset)
 
-    def __getitem__(self, id):
-        image = Image.fromarray(self.images[id].detach().numpy())
-        label = self.labels[int(self.image_labels[id])]
+    def __getitem__(self, i):
+        image = Image.fromarray(self.images[i].detach().numpy())
+        label = self.category_labels[int(self.image_categories[i])]
 
         return image, label
--- src/cgan/model/discriminator.py
+++ src/cgan/model/discriminator.py
@@ -1,28 +1,55 @@
+import torch
 import torch.nn as nn
 
 
 class Discriminator(nn.Module):
-    def __init__(self):
+    def __init__(self, n_pixels=784, n_labels=10):
         super().__init__()
 
-        self.label_emb = nn.Embedding(10, 10)
-
+        # From what I can gather, this is an overcomplicated way of making dummy variables.
+        # The dataset of the code where we saw this has a number 0-9 as the first column.
+        # What this embedding does is basically the same as making dummy variables [0,...,1]. Shape: (10).
+        # The output from this seems random, but is the same if the input is the same.
+        # I assume it therefore works since the model can consider these numbers instead of [0,...,1].
+        # >>> le = nn.Embedding(3, 3)
+        # >>> le(torch.tensor([0,0,1,1,2,2]))
+        # tensor([[-0.9402, -1.5847, -0.3980], These are the same...
+        #         [-0.9402, -1.5847, -0.3980],  ^
+        #         [ 1.3810, -0.2916, -2.3109], these too...
+        #         [ 1.3810, -0.2916, -2.3109],  ^
+        #         [ 0.2044,  1.9303, -0.9796], and these
+        #         [ 0.2044,  1.9303, -0.9796]]) ^
+        # Question: Is this really the best way to do this?
+        self.label_embedding = nn.Embedding(n_labels, n_labels)
         self.model = nn.Sequential(
-            # big todo
-
-            # Kokt kode
-            nn.Linear(794, 1024),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.3),
-            nn.Linear(1024, 512),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.3),
-            nn.Linear(512, 256),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Dropout(0.3),
+            *self.hidden_layer(n_pixels + n_labels, 1024),
+            *self.hidden_layer(1024, 512),
+            *self.hidden_layer(512, 256),
             nn.Linear(256, 1),
             nn.Sigmoid()
         )
 
-    def forward(self, x, labels):
-        print("tood")  # todo
+    def forward(self, images, labels):
+        """
+        Takes a list of images and labels and returns the probability of them being real.
+        :param images: Tensor representation of n images. Shape: (n, sqrt(n_pixels), sqrt(n_pixels))
+        :param labels: Labels belonging to the images. Shape: (n)
+        :return: Probability of each image being real. Shape: (n)
+        """
+        n_images = images.size(0)
+        images = images.view(n_images, 784)
+        label_dummies = self.label_embedding(labels)  # A number -> List of numbers. Shape: (n_labels)
+
+        # Add the label dummy variables to the images array before inputting into the model.
+        images = torch.cat([images, label_dummies], 1)
+
+        return self.model(images).squeeze()  # Squeeze remove all single element dimensions.
+
+    @staticmethod
+    def hidden_layer(in_features: int, out_features: int):
+        negative_slope, dropout = 0.2, 0.3
+        return (
+            nn.Linear(in_features, out_features),
+            nn.LeakyReLU(negative_slope, inplace=True),
+            nn.Dropout(dropout)
+        )
