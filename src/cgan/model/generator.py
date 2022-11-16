9da3849364770a322204f43657f8356df624c60a
Aleksander Halvorsen Holthe <alekhh123@gmail.com>
2022-11-16 15:45:58 +0100

    Groundwork for more layers and stuff

--- src/cgan/model/generator.py
+++ src/cgan/model/generator.py
@@ -11,13 +11,15 @@ class Generator(nn.Module):
         self.img_pixels_out = img_size_out**2
         self.n_labels = n_labels
         self.kernel_size = kernel_size
+        
+        self.negative_slope = 0.2
+        self.embedding_size = 10
 
-        self.embedding_size = int(0.5 * self.n_labels)
-        self.label_embedding = nn.Embedding(self.n_labels, int(0.5 * self.n_labels))
+        self.label_embedding = nn.Embedding(self.n_labels, self.embedding_size)
 
         self.model = nn.Sequential(
             # f(*(a, b, c)) equals f(a, b, c)
-            *self.first_layer(
+            *self.noise_layer(
                 self.img_pixels_in + self.embedding_size, self.img_pixels_in
             ),
             *self.hidden_layer(self.img_pixels_in, 512, self.kernel_size),
@@ -26,25 +28,41 @@ class Generator(nn.Module):
             *self.last_layer(2048, self.img_pixels_out, self.kernel_size),
         )
 
-    @staticmethod
-    def first_layer(in_features, out_features):
-        negative_slope = 0.2
-        return (
+        self.noise_layer = nn.Sequential(
             # Dense layer xW + b (see lecture 1: linear regression)
-            # TODO: Is this a sensible way to bake the label embedding into the data? Probably not.
-            nn.Linear(in_features, out_features),
-            nn.LeakyReLU(negative_slope, inplace=True),
+            nn.Linear(self.img_pixels_in, self.img_pixels_out/4),
+            # LeakyReLU is like ReLU but negative values get shrunk, not removed
+            nn.LeakyReLU(self.negative_slope, inplace=True)
         )
+
+        self.label_layer = nn.Sequential(
+            # Embedding layer takes a label number 0-n and returns an array of numbers
+            nn.Embedding(self.n_labels, self.embedding_size),
+            nn.Linear(self.embedding_size, self.img_pixels_out/4),
+            nn.LeakyReLU(self.negative_slope, inplace=True)
+        )
+
+    def apply_noise_layer(self, n_images, noise):
+        # Reshape the noise to be single strip of pixels
+        noise = noise.view(n_images, self.img_pixels_in)
+
+        # Apply the layer to the data
+        noise = self.noise_layer(noise)
+
+        return noise.view(n_images, self.img_pixels_out/4*7*7, 7, 7)
+        
+    
+    def apply_label_layer(self, n_images, labels):
+        return self.label_layer(labels).view()  # TODO
 
     @staticmethod
     def hidden_layer(in_features, out_features, kernel_size):
-        negative_slope = 0.2
         return (
             # Convolutional layer (see lecture 3: CNN)
             # TODO: This is not in_features, it's in_channels. Very different.
             nn.Conv2d(in_features, out_features, kernel_size),
             # LeakyReLU is like ReLU but negative values get shrunk, not removed.
-            nn.LeakyReLU(negative_slope, inplace=True),
+            nn.LeakyReLU(self.negative_slope, inplace=True),
         )
 
     @staticmethod
@@ -57,8 +75,9 @@ class Generator(nn.Module):
         n_images = noise.size(0)
 
         # Reshape the noise to be single strip of pixels
-        # TODO: We can't do this if we are going to send a 2D picture through the model.
         noise = noise.view(n_images, self.img_pixels_in)
+
+        noise = self.noise_layer(10*10, 7*7*10)(noise)
 
         # Add the label embedding to each image [Image|Embedding]
         # TODO: If we are going to send 10*10 pixels into the conv layers, how will embedding work?
